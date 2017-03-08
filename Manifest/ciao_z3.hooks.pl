d3406f475fa11ccd276c15622de3c657fdd7fd0f
Jose F. Morales <jfmcjf@gmail.com>
2017-03-08 19:43:27 +0100

    Simpler 3rd-party installation code (needs latest Ciao version)

--- Manifest/ciao_z3.hooks.pl
+++ Manifest/ciao_z3.hooks.pl
@@ -1,11 +1,15 @@
 :- module(_, [], [ciaobld(bundlehooks)]).
 
-:- doc(title,  "Bundle Hooks for Ciao-Z3").
+:- doc(title, "Bundle Hooks for Ciao-Z3").
+
+:- use_module(library(bundle/bundle_paths), [bundle_path/3]).
+:- use_module(ciaobld(builder_aux), [third_party_aux/3]).
 
 '$builder_hook'(prepare_build_bin) :-
-	aux_call(['install_bin_dist']), % Use binary distribution
-%	aux_call(['install_src_dist']), % Use source distribution
-	aux_call(['gen_conf']).
+	third_party_aux(ciao_z3, z3, ['install_bin_dist']), % Use binary distribution
+%	third_party_aux(ciao_z3, z3, ['install_src_dist']), % Use source distribution
+	Conf = ~bundle_path(ciao_z3, 'src/ciao_z3_config_auto.pl'),
+	third_party_aux(ciao_z3, z3, ['gen_conf', Conf]).
 
 '$builder_hook'(lib('src')).
 
@@ -17,23 +21,3 @@
 '$builder_hook'(test) :- !,
 	% TODO: check output programmatically
 	runtests_dir(ciao_z3, 'tests', [rtc_entry, dump_output]).
-
-% ---------------------------------------------------------------------------
-% (call external scripts and makefiles)
-
-:- use_module(library(process), [process_call/3]).
-:- use_module(library(bundle/bundle_paths), [bundle_path/3]).
-:- use_module(library(bundle/bundle_flags), [get_bundle_flag/2]).
-:- use_module(ciaobld(third_party_install), [third_party_path/2]).
-
-% (will not work in Windows)
-aux_sh := ~bundle_path(ciao_z3, 'Manifest/hooks.sh').
-
-aux_call(Args) :- 
-	OS = ~get_bundle_flag(core:os),
-	Arch = ~get_bundle_flag(core:arch),
-	third_party_path(prefix, ThirdParty), % TODO: add bundle to third_party_path/2
-	Env = ['CIAO_OS'=OS, 'CIAO_ARCH'=Arch, 'THIRDPARTY'=ThirdParty],
-	process_call(~aux_sh, Args, [env(Env)]).
-
-
